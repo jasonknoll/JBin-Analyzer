a290fea682f3ee2600943bc99e532cae146ab062
jasonknoll <jasonknoll98@aol.com>
2023-08-31 17:07:29 +0000

    Added TODO section

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    
+    /* TODO
+    - Import file to be analyzed
+    - Grab attribute data off of it
+        + Size, creation date, etc.
+    - Hash/calculate checksum and save somewhere (idk)
+    - Extract available strings
+    - Find out how to disassemble an exe lol
+    - Add a console GUI
+    */
 }
