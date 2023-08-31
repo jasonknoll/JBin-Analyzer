b64b36d683131347f1e6440a6bf05639dcc32139
jasonknoll <jasonknoll98@aol.com>
2023-08-31 20:19:04 +0000

    Added pattern matching to check for file argument in the 2nd position

--- src/main.rs
+++ src/main.rs
@@ -16,16 +16,29 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    println!("{:?}", args);
-
     // If file path is provided, great.
     // If not, wait and then ask to set it in-app.
     let mut file_path: Option<&Path> = None;
 
     // check for argument
-
+    file_path = match args.len() {
+        2 => Some(get_file_path(&args[1])),
+        _ => None,
+    };
+    
     // Get basic attributes
-    match fs::metadata(&file_path) {
+    match file_path {
+        None => println!("No file path detected"),
+        _ => get_file_metadata(&file_path.unwrap())
+    }
+}
+
+fn get_file_path(path: &String) -> &Path{
+    return Path::new(path);
+}
+
+fn get_file_metadata(path: &Path){
+    match fs::metadata(&path) {
         Ok(metadata) => {
             let size = metadata.len();
 
